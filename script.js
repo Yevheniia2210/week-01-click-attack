e979b2e03bc1a03f38472160122a0ecdc84a739a
Yevheniia Pavliukova <foxyluko22@gmail.com>
2026-09-22 12:24:58 +0000

    Add Power Attack

--- script.js
+++ script.js
@@ -3,8 +3,23 @@ let score = 0;
 const scoreDisplay = document.getElementById("score");
 const title = document.getElementById("title");
 const attackButton = document.getElementById("attackButton");
+const powerAttackButton = document.getElementById("powerAttackButton");
 const resetButton = document.getElementById("resetButton");
 
-// TODO: create addPoint()
-// TODO: create resetGame()
-// TODO: connect both functions to buttons
+function addPoint() {
+    score = score + 1;
+    scoreDisplay.textContent = score;
+}
+/function resetGame() {
+score = 0;
+scoreDisplay.textContent = score;
+}
+
+function powerAttack() {
+    score = score + 5;
+    scoreDisplay.textContent = score;
+}
+
+attackButton.addEventListener("click", addPoint);
+resetButton.addEventListener("click", resetGame);
+powerAttackButton.addEventListener("click", powerAttack);
